385d62d25dbc643a087ef531b0d4f5689382f299
TaylorGoulding <tototianhao@gmail.com>
2023-07-04 03:11:04 +0000

    add

--- utils/cmd.py
+++ utils/cmd.py
@@ -2,6 +2,8 @@ import subprocess
 
 def gitclone(url, recursive=False, dest=None):
     command = ['git', 'clone', url]
+    if dest: command.append(dest)
+    if recursive: command.append('--recursive')
     res = subprocess.run(command, stdout=subprocess.PIPE).stdout.decode('utf-8')
     print(res)
 
@@ -10,13 +12,18 @@ def pipi(modulestr):
     res = subprocess.run(['python','-m','pip', '-q', 'install', modulestr], stdout=subprocess.PIPE).stdout.decode('utf-8')
     print(res)
 
+
+def pipie(modulestr):
+    res = subprocess.run(['python','-m','pip', 'install', '-e', modulestr], stdout=subprocess.PIPE).stdout.decode('utf-8')
+    print(res)
+
 def pipis(modulestrs:list):
     pip_cmd = ['python','-m','pip', '-q', '-y','install']
     pips_cmd = pip_cmd + modulestrs
     res = subprocess.run(pips_cmd, stdout=subprocess.PIPE).stdout.decode('utf-8')
     print(res)
 
-def pipie(modulestr):
+def gite(modulestr):
     res = subprocess.run(['git', 'install', '-e', modulestr], stdout=subprocess.PIPE).stdout.decode('utf-8')
     print(res)
 
