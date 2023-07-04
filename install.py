54ce2df1f0c1caf74ca2aca40e0f501dbda5f8d7
toto <tototianhao@gmail.com>
2023-07-04 11:36:38 +0800

    installpy

--- install.py
+++ install.py
@@ -1,11 +1,18 @@
 import subprocess
 import os
-from utils.cmd import pipi, pipie, pipis, gitclone
+from utils.cmd import pipi, pipie, pipis, gitclone, unpip
 
 from utils.nvidia_utils import print_card
 
 if __name__ == "__main__":
     print_card()
+    pipis(['tqdm','ipwidgets==7.7.1','protobuf==3.20.3'])
+
+    from tqdm.notebook import tqdm
+    progress_bar = tqdm(total= 25)
+    progress_bar.set_description("Install dependencies")
+    pipis(['mediapipe','piexif','safetensors','lark'])
+    unpip('torchtext')
     gitclone('https://github.com/sxela/sxela-stablediffusion',
              dest='stablediffusion')
     gitclone('https://github.com/sxela/controlnet-v1-1-nightly',
@@ -27,4 +34,17 @@ if __name__ == "__main__":
         os.chdir(f'../')
     except:
         pass
+    progress_bar.update(2)
+    pipi('Pillow==9.0.0')
     pipie('./stablediffusion')
+    progress_bar.update(2)
+    pipis(['ipywidgets==7.7.1','transformers==4.19.2','omegaconf','einops','pytorch_lightning>1.4.11,<=1.7.7', 'scikit-image','opencv-python'])
+    progress_bar.update(2)
+    progress_bar.update(2)
+    progress_bar.update(2)
+    progress_bar.update(2)
+    progress_bar.update(2)
+    progress_bar.update(2)
+    progress_bar.update(2)
+    progress_bar.update(2)
+    progress_bar.update(2)
--- utils/cmd.py
+++ utils/cmd.py
@@ -12,13 +12,17 @@ def pipi(modulestr):
     res = subprocess.run(['python','-m','pip', '-q', 'install', modulestr], stdout=subprocess.PIPE).stdout.decode('utf-8')
     print(res)
 
+def unpip(modulestr):
+    res = subprocess.run(['python','-m','pip', '-q', 'uninstall', modulestr, '-y'], stdout=subprocess.PIPE).stdout.decode('utf-8')
+    print(res)
+
 
 def pipie(modulestr):
     res = subprocess.run(['python','-m','pip', 'install', '-e', modulestr], stdout=subprocess.PIPE).stdout.decode('utf-8')
     print(res)
 
 def pipis(modulestrs:list):
-    pip_cmd = ['python','-m','pip', '-q', '-y','install']
+    pip_cmd = ['python','-m','pip', '-y','install']
     pips_cmd = pip_cmd + modulestrs
     res = subprocess.run(pips_cmd, stdout=subprocess.PIPE).stdout.decode('utf-8')
     print(res)
