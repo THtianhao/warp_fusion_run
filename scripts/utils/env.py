714f699e4d4e35340759c0a0b5eafd02d53bfe2e
TaylorGoulding <tototianhao@gmail.com>
2023-07-11 11:53:43 +0000

    update controlnet

--- scripts/utils/env.py
+++ scripts/utils/env.py
@@ -10,6 +10,7 @@ sys.path.append(f'{root_dir}/k-diffusion')
 sys.path.append(f'{root_dir}/guided-diffusion')
 sys.path.append(f'{root_dir}/ResizeRight')
 sys.path.append(f'{root_dir}/BLIP')
+sys.path.append(f'{root_dir}/ControlNet')
 
 sys.path.append(root_path)
 initDirPath = os.path.join(root_path, 'init_images')
