aeecc330488ac2df1bb7b9bd705f2f106b2cf861
toto <tototianhao@gmail.com>
2023-07-11 19:24:53 +0800

    修改normal方法

--- scripts/settings/setting.py
+++ scripts/settings/setting.py
@@ -67,5 +67,5 @@ warp_interp = Image.BILINEAR
 DEVICE = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 print('Using device:', DEVICE)
 device = DEVICE  # At least one of the modules expects this name..
-normalize = T.Normalize(mean=[0.48145466, 0.4578275, 0.40821073], std=[0.26862954, 0.26130258, 0.27577711])
+normalize = T.normalize(mean=[0.48145466, 0.4578275, 0.40821073], std=[0.26862954, 0.26130258, 0.27577711])
 lpips_model = lpips.LPIPS(net='vgg').to(device)
